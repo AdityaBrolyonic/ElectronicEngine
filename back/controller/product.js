3648727146d7015b0610da1ea584b4b629d07f5e
Aditya <adityakakarot45@gmail.com>
2025-07-28 22:33:21 +0530

    starting_front

--- back/controller/product.js
+++
@@ -1,7 +0,0 @@
-// const express = require("express");
-// const { createProduct } = require("../routes/product");
-// const router = require('express').Router()
-
-// router.post("/createProduct", createProduct);
-
-// module.exports = router
